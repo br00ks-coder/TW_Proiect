e72fa22e504a70260da118f2b3c5047a8d2b4469
FILIMON Mihail <mih.fil76@gmail.com>
2023-06-30 16:47:08 +0300

    Test Commit

---
+++ Model/FlowerModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Model;
+
+require_once PROJECT_ROOT_PATH . "/Model/PDOConn.php";
+
+class FlowerModel extends PDOConn
+{
+    public function getFlowers($limit)
+    {
+        return $this->selectAll("SELECT * FROM flowers ORDER BY id ASC LIMIT :limit", ['limit' => $limit]);
+    }
+}
+
