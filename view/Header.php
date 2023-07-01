320b4914c6f1f3fd5e534b5ed9b0746145013411
Catalin Curlea <cataling.780@gmail.com>
2023-07-01 03:48:44 +0300

    fixed navbar for phone

--- view/Header.php
+++ view/Header.php
@@ -22,7 +22,7 @@
             <?php if ($validationResult): ?>
                 <!-- Display content for logged-in users -->
 
-<section>
+
     <li id="fav-window" style="display: none;">
         <h2>Favorites</h2>
         <ul id="fav-items">
@@ -39,7 +39,7 @@
     </li>
     <button id="cart_button" onclick="toggleCartWindow()"><i class="fa fa-shopping-cart" ></i></button>
 
-</section>
+
 
                 <a href="../profile.php">
                     <li class="profile">Profile</li>
@@ -114,27 +114,71 @@
 
     </nav>
     <nav id="nav_for_media">
-        <ul><i class="fa-solid fa-xmark" id="close_menu"></i>
-            <a href="../login.php">
-                <li class="log_in">Log In</li>
-            </a>
-            <a href="../register.php">
-                <li class="register">Register</li>
-            </a>
-            <a href="../index.php#main">
-                <li class="about_us">About Us</li>
-            </a>
-            <a href="../buy.php">
-                <li class="buy_flowers">Buy Flowers</li>
-            </a>
 
-            <a href="../help.php" >
-                <li class="contact_button"> Get Help</li>
-            </a>
+            <?php if ($validationResult): ?>
+                <ul><i class="fa-solid fa-xmark" id="close_menu"></i>
 
-            <a href="#footer">
-                <li class="contact_button">Contact Us</li>
-            </a>
+                    <a href="../about.php">
+                        <li class="about_us">About</li>
+                    </a>
+                    <a href="../check.php">
+                        <li class="check_flowers">Warehouse</li>
+                    </a>
+                    <a href="../buy.php">
+                        <li class="buy_flowers">Shop</li>
+                    </a>
+                    <a href="../humidify.php">
+                        <li class="humidify">Garden</li>
+                    </a>
+
+                    <a href="../contact.php">
+                        <li class="contact_button">Contact</li>
+                    </a>
+                    <a href="../help.php" >
+                        <li class="contact_button"> Get Help</li>
+                    </a>
+                    <a href="../php/logout.php">
+                        <li class="logout">Log out</li>
+                    </a>
+
+                        <li id="fav-window" style="display: none;">
+                            <h2>Favorites</h2>
+                            <ul id="fav-items">
+                            </ul>
+                        </li>
+                        <button id="cart_button" onclick="location.href = '../buy.php';"><i class="fa-solid fa-heart"></i></button>
+
+
+                        <li id="cart-window" style="display: none;">
+                            <h2>Cart</h2>
+                            <ul id="cart-items">
+                            </ul>
+                            <p>Total: $<span id="cart-total">0.00</span></p>
+                        </li>
+                        <button id="cart_button" onclick="location.href = '../checkout.php';"><i class="fa fa-shopping-cart" ></i></button>
+
+
+                </ul>
+            <?php else: ?>
+
+                <ul>
+
+                    <a href="../about.php">
+                        <li class="about_us">About</li>
+                    </a>
+                    <a href="../buy.php">
+                        <li class="buy_flowers">Shop</li>
+
+                        <a href="../contact.php">
+                            <li class="contact_button">Contact</li>
+                        </a>
+                        <a href="../help.php" >
+                            <li class="contact_button"> Get Help</li>
+                        </a>
+
+                </ul>
+
+            <?php endif; ?>
         </ul>
     </nav>
 
